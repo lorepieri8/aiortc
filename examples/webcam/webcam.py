4aaa5da9a1a28be74d57880885fd3ef48d209d3c
lorepieri8 <lorepieri@hotmail.it>
2021-11-03 00:02:28 +0100

    Update webcam.py

--- examples/webcam/webcam.py
+++ examples/webcam/webcam.py
@@ -36,7 +36,7 @@ def create_local_tracks(play_from):
                     "video=Integrated Camera", format="dshow", options=options
                 )
             else:
-                webcam = MediaPlayer("/dev/video0", format="v4l2", options=options)
+                webcam = MediaPlayer("/dev/video6", format="v4l2", options=options)
             relay = MediaRelay()
         return None, relay.subscribe(webcam.video)
 
